79959e46df7b4e696fd09bc2251497fa0676e38b
moinuddin3188 <moinuddin3188@gmail.com>
2023-07-16 19:25:27 +0600

    [DONE] Sign up functional

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,68 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { FormEvent, useEffect, useState } from "react";
 import book from "../assets/book.png";
+import {
+  useLoginMutation,
+  useSignUpMutation,
+} from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hook";
+import { useNavigate } from "react-router-dom";
+import { userLogin } from "../redux/features/auth/authSlice";
 
 export default function Signup() {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [signUp, { data, isLoading, isSuccess }] = useSignUpMutation();
+  const [login, { data: loginData, isSuccess: loginSuccess }] =
+    useLoginMutation();
+
+  const handleSignUp = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    void signUp({
+      name: {
+        firstName,
+        lastName,
+      },
+      email,
+      password,
+    });
+  };
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSuccess) {
+      void login({ email, password });
+    }
+  }, [data, isSuccess]);
+
+  useEffect(() => {
+    if (loginSuccess) {
+      dispatch(
+        userLogin({
+          accessToken: loginData?.data?.accessToken,
+          user: { email, role: "user" },
+        })
+      );
+    }
+
+    localStorage.setItem(
+      "auth",
+      JSON.stringify({
+        accessToken: data.data.accessToken,
+        user: { email: email, role: "user" },
+      })
+    );
+
+    navigate("/");
+  }, [loginSuccess, loginData]);
+
   return (
     <>
       <div className="grid grid-cols-2 gap-0 h-screen">
@@ -10,14 +72,17 @@ export default function Signup() {
         <div className="flex items-center justify-center">
           <div className="w-2/5">
             <h1 className="text-center text-2xl font-bold mb-4">Sign up</h1>
-            <form action="" className="bg-white shadow-lg rounded-md p-4">
+            <form
+              onSubmit={handleSignUp}
+              className="bg-white shadow-lg rounded-md p-4"
+            >
               <div className="mb-4">
                 <label htmlFor="name" className="block text-gray-700">
                   First Name
                 </label>
                 <input
+                  onChange={(e) => setFirstName(e.target.value)}
                   type="text"
-                  id="name"
                   name="name"
                   className="form-input mt-1 block w-full bg-gray-100 h-9 rounded-md border"
                   required
@@ -28,8 +93,8 @@ export default function Signup() {
                   Last Name
                 </label>
                 <input
+                  onChange={(e) => setLastName(e.target.value)}
                   type="text"
-                  id="name"
                   name="name"
                   className="form-input mt-1 block w-full bg-gray-100 h-9 rounded-md border"
                   required
@@ -41,8 +106,8 @@ export default function Signup() {
                   Email
                 </label>
                 <input
+                  onChange={(e) => setEmail(e.target.value)}
                   type="email"
-                  id="email"
                   name="email"
                   className="form-input mt-1 block w-full bg-gray-100 h-9 rounded-md border"
                   required
@@ -54,8 +119,8 @@ export default function Signup() {
                   Password
                 </label>
                 <input
+                  onChange={(e) => setPassword(e.target.value)}
                   type="password"
-                  id="password"
                   name="password"
                   className="form-input mt-1 block w-full bg-gray-100 h-9 rounded-md border"
                   required
@@ -64,6 +129,7 @@ export default function Signup() {
 
               <div className="flex justify-center">
                 <button
+                  disabled={isLoading}
                   type="submit"
                   className="bg-secondary text-white py-2 px-4 rounded hover:bg-secondary-focus"
                 >
